8a126b3dfa2f478df8f0d10572c8faea982afe39
Federico Brigante <opensource@bfred.it>
2020-04-19 17:35:14 +0200

    Move text direction picker to CSS
    The `lang` attribute was also incorrect. Just because the browser UI is set to a language it doesn't mean that *our* UI uses that language.

--- source/main.js
+++ source/main.js
@@ -5,6 +5,4 @@ new App({
 });
 
 // Move to App.svelte once this is supported: https://github.com/sveltejs/svelte/issues/3105
-document.documentElement.setAttribute('lang', browser.i18n.getUILanguage());
-document.documentElement.setAttribute('dif', browser.i18n.getMessage('@@bidi_dir'));
 document.body.dataset.type = new URLSearchParams(location.search).get('type');
