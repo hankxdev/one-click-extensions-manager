ae0111f98caf556a6d2e76359da67a68b6f0d5f1
Federico Brigante <me@fregante.com>
2022-10-07 13:08:51 +0700

    DRY

--- source/lib/focus-next.js
+++ source/lib/focus-next.js
@@ -1,10 +1,18 @@
-export function focusNext(selector) {
+function focus(selector, next) {
 	const {activeElement} = document;
 	const items = [...document.querySelectorAll(selector)];
+	if (!next) {
+		items.reverse();
+	}
+
 	for (const item of items) {
 		const position = activeElement.compareDocumentPosition(item);
-		// eslint-disable-next-line no-bitwise -- https://developer.mozilla.org/en-US/docs/Web/API/Node/compareDocumentPosition
-		if (position & Node.DOCUMENT_POSITION_FOLLOWING) {
+		if (
+			position &
+			(next
+				? Node.DOCUMENT_POSITION_FOLLOWING
+				: Node.DOCUMENT_POSITION_PRECEDING)
+		) {
 			item.focus();
 			return;
 		}
@@ -13,17 +21,10 @@ export function focusNext(selector) {
 	items.at(0).focus();
 }
 
-export function focusPrevious(selector) {
-	const {activeElement} = document;
-	const items = [...document.querySelectorAll(selector)].reverse();
-	for (const item of items) {
-		const position = activeElement.compareDocumentPosition(item);
-		// eslint-disable-next-line no-bitwise -- https://developer.mozilla.org/en-US/docs/Web/API/Node/compareDocumentPosition
-		if (position & Node.DOCUMENT_POSITION_PRECEDING) {
-			item.focus();
-			return;
-		}
-	}
+export function focusNext(selector) {
+	focus(selector, true);
+}
 
-	items.at(0).focus();
+export function focusPrevious(selector) {
+	focus(selector, false);
 }
