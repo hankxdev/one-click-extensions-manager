76193dde38367be8ccc3bf1feae40c27a06b0d61
Federico Brigante <me@fregante.com>
2023-03-18 00:29:38 +0800

    Try `openingStyle`

--- source/options-storage.js
+++ source/options-storage.js
@@ -1,9 +1,11 @@
 import OptionsSync from 'webext-options-sync';
 
 const optionsStorage = new OptionsSync({
+	storageType: 'local', // `openingStyle` cannot be synchronized
 	defaults: {
 		showButtons: 'on-demand', // Or 'always'
 		width: '',
+		openingStyle: 'popup',
 	},
 });
 
--- source/options/options.js
+++ source/options/options.js
@@ -1,3 +1,21 @@
 import optionsStorage from '../options-storage.js';
 
 optionsStorage.syncForm(document.querySelector('form'));
+
+function openTab() {
+	chrome.tabs.create({url: 'index.html'});
+}
+
+document
+	.querySelector('#openingStyle')
+	.addEventListener('change', async ({target}) => {
+		const isPopup = target.value === 'popup';
+		await chrome.action.setPopup({
+			popup: isPopup ? 'index.html?type=popup' : '',
+		});
+		if (isPopup) {
+			chrome.action.onClicked.removeListener(openTab);
+		} else {
+			chrome.action.onClicked.addListener(openTab);
+		}
+	});
