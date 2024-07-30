52c3dce2cdf956d41c6354b7eb5ad234fc3a62df
Federico <me@fregante.com>
2024-07-30 18:46:04 +0700

    Last

--- source/lib/undo-stack.js
+++ source/lib/undo-stack.js
@@ -4,20 +4,17 @@ export default class UndoStack {
 	static isMac = navigator.platform.includes('Mac');
 	static replaceKbdOnMac = string =>
 		UndoStack.isMac
-			? // Some locales don't call it "ctrl"
-			string.replace(/(?<=>)[a-z]+\+z/i, '⌘Z')
+			? string.replace(/(?<=>)[a-z]+\+z/i, '⌘Z') // Some locales don't call it "ctrl"
 			: string;
 
 	constructor(element) {
-		if (element) {
-			element.addEventListener('keydown', this.#keyboardEventListener);
-		}
+		element?.addEventListener('keydown', this.#keyboardEventListener);
 	}
 
 	#keyboardEventListener = event => {
 		if (
-			event.code === 'KeyZ'
-			&& (UndoStack._isMac ? event.metaKey : event.ctrlKey)
+			event.code === 'KeyZ' &&
+			(UndoStack._isMac ? event.metaKey : event.ctrlKey)
 		) {
 			if (event.shiftKey) {
 				this.redo();
