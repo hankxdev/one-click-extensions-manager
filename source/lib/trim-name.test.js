788ddd2b5617811faa40b8b2c7058d7bd734c240
Federico <me@fregante.com>
2024-09-27 12:09:41 +0700

    Drop Vitest dependency, if you prefer

--- source/lib/trim-name.test.js
+++ source/lib/trim-name.test.js
@@ -1,27 +1,30 @@
-import {expect, it} from 'vitest';
+import assert from 'node:assert';
+import it from 'node:test';
+
 import trimName from './trim-name.js';
 
 it('preserves regular names', () => {
-	expect(trimName('Regular name')).toBe('Regular name');
-	expect(trimName('One-click extension manager')).toBe(
+	assert.equal(trimName('Regular name'), 'Regular name');
+	assert.equal(
+		trimName('One-click extension manager'),
 		'One-click extension manager',
 	);
 });
 
 it('trims punctuation', () => {
-	expect(trimName('Dash — extension that does stuff')).toBe('Dash');
-	expect(trimName('Em Dash — useless information')).toBe('Em Dash');
-	expect(trimName('Semicolon: Do things')).toBe('Semicolon');
-	expect(trimName('French semicolon : Do things')).toBe('French semicolon');
+	assert.equal(trimName('Dash — extension that does stuff'), 'Dash');
+	assert.equal(trimName('Em Dash — useless information'), 'Em Dash');
+	assert.equal(trimName('Semicolon: Do things'), 'Semicolon');
+	assert.equal(trimName('French semicolon : Do things'), 'French semicolon');
 });
 
 it('trims keywords', () => {
-	expect(trimName('Cake Search Extension')).toBe('Cake Search');
-	expect(trimName('Coconuts for Chrome')).toBe('Coconuts');
-	expect(trimName('Electric Fans for Google Chrome')).toBe('Electric Fans');
-	expect(trimName('Blonde browser extension')).toBe('Blonde');
+	assert.equal(trimName('Cake Search Extension'), 'Cake Search');
+	assert.equal(trimName('Coconuts for Chrome'), 'Coconuts');
+	assert.equal(trimName('Electric Fans for Google Chrome'), 'Electric Fans');
+	assert.equal(trimName('Blonde browser extension'), 'Blonde');
 });
 
 it('never returns an empty string', () => {
-	expect(trimName('Chrome Extension')).toBe('Chrome Extension');
+	assert.equal(trimName('Chrome Extension'), 'Chrome Extension');
 });
