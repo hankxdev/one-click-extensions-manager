eb7f0b6aa27bf1333c7443ef78c8de3103e2ab71
Federico <me@fregante.com>
2026-06-12 14:54:01 +0800

    Code cleanup

--- source/options-storage.js
+++ source/options-storage.js
@@ -5,7 +5,7 @@ const optionsStorage = new OptionsSync({
 		position: 'popup',
 		showButtons: 'on-demand', // Or 'always'
 		width: '',
-		pinnedExtensions: [], // Array of pinned extension IDs
+		pinnedExtensions: [],
 	},
 	migrations: [
 		options => {
@@ -20,34 +20,10 @@ const optionsStorage = new OptionsSync({
 
 			options.width = Number.isNaN(width) ? '' : width;
 		},
-		// Migration to add pinnedExtensions if it doesn't exist
-		options => {
-			if (!options.pinnedExtensions) {
-				options.pinnedExtensions = [];
-			}
-		},
 	],
 });
 
 export default optionsStorage;
-
-// Helper functions for managing pinned extensions
-export async function togglePin(extensionId) {
-	const options = await optionsStorage.getAll();
-	const pinnedExtensions = [...options.pinnedExtensions];
-	const index = pinnedExtensions.indexOf(extensionId);
-
-	if (index > -1) {
-		// Unpin
-		pinnedExtensions.splice(index, 1);
-	} else {
-		// Pin
-		pinnedExtensions.push(extensionId);
-	}
-
-	await optionsStorage.set({pinnedExtensions});
-	return index === -1; // Return true if pinned, false if unpinned
-}
 
 const defaultPopup = chrome.runtime.getManifest().action.default_popup;
 
