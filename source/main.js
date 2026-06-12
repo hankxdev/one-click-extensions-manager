dc7b1f3cd778c9198bd6ee9b7cc0840ce79f5824
Federico <me@fregante.com>
2026-06-12 16:40:54 +0800

    Update main.js

--- source/main.js
+++ source/main.js
@@ -1,11 +1,9 @@
+import {mount} from 'svelte';
 import App from './app.svelte';
 import fitWindow from './lib/fit-window.js';
 import preventMultipleWindows from './lib/single-window.js';
 
-// eslint-disable-next-line no-new -- Tell this to Svelte
-new App({
-	target: document.body,
-});
+mount(App, {target: document.body});
 
 const autoFit = new URLSearchParams(globalThis.location.search).has('auto-fit');
 if (autoFit) {
